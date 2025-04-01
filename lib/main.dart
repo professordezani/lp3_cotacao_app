674379440c64916d2d488822c4b47a33c85373f9
Henrique Dezani <professorhenriquedezani@gmail.com>
2025-04-01 20:20:04 -0300

    adiciona outras moedas

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,31 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
 
+class _MyAppState extends State<MyApp> {
   var txtReais = TextEditingController();
   var txtDolares = TextEditingController();
+  var txtMoeda = TextEditingController();
+  var moeda = "USD-BRL";
 
   void converter () async {
 
-    var url = 'https://economia.awesomeapi.com.br/json/last/USD-BRL';
+    var url = 'https://economia.awesomeapi.com.br/json/last/$moeda';
 
     var response = await http.get(Uri.parse(url));
 
     var dados = json.decode(response.body);
 
-    double cotacao = double.parse(dados["USDBRL"]["ask"]);
+    double cotacao = double.parse(dados[moeda.replaceFirst("-", "")]["ask"]);
     double reais = double.parse(txtReais.text);
     double dolares = reais / cotacao;
     txtDolares.text = dolares.toString();
@@ -41,10 +49,21 @@ class MyApp extends StatelessWidget {
               ),
             ),
             SizedBox(height: 10),
+            DropdownMenu(
+              width: MediaQuery.of(context).size.width - 20,
+              onSelected: (value) => setState(() => moeda = value!),
+              controller: txtMoeda,
+              dropdownMenuEntries: [
+                DropdownMenuEntry(label: "Dólar", value: "USD-BRL"),
+                DropdownMenuEntry(label: "Euro", value: "EUR-BRL"),
+                DropdownMenuEntry(label: "Bitcoin", value: "BTC-BRL"),
+              ]
+            ),
+            SizedBox(height: 10),
             TextField(
               controller: txtDolares,
               decoration: InputDecoration(
-                labelText: "Dólar",
+                labelText: txtMoeda.text,
                 border: OutlineInputBorder()
               ),
             ),
